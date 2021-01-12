f6bb8bd0d6d9142b965b9a126771bcb05b26f88a
dalofulgence <dal19006@byui.edu>
2021-01-12 06:54:38 -0700

    added new index page

---
+++ mystyle.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("load", (event)=>{
+    const lu = document.querySelector("#lastupdated");
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+})
