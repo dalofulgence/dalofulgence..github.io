526618b39bae43803c415d8a0e1d88afd930c859
dalofulgence <dal19006@byui.edu>
2021-03-13 21:35:15 -0700

    update -js

--- lesson5/preston.js
+++ lesson5/preston.js
@@ -13,20 +13,23 @@ else {
     element.style.display = "none";
 
 }
+} 
+
+/*var header = document.getElementById("navigation");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}*/
 
 const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=f5597f72a3504f64f27f4c96c72b88cc&units=imperial";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
-
-
-
-    /*const days = jsonObject['sunday', 'Monday', 'Teasday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    /*let days = document.createElement('section')
-    /*let forecast = document.createElement('section');
-    /*let icon = document.createElement('icon');
-    /*let degree = document.createElement('')*/
+    console.table(jsObject);
     
     document.getElementById('currently').textContent = jsObject.list[4].weather[0].description;
     document.getElementById('high').textContent = jsObject.list[4].main.temp;
