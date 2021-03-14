24d1444fd57003aa48d35682a93558f328437e82
dalofulgence <dal19006@byui.edu>
2021-03-13 21:31:51 -0700

    lesson 10

--- lesson5/preston.js
+++ lesson5/preston.js
@@ -13,14 +13,35 @@ else {
     element.style.display = "none";
 
 }
-} 
 
-/*var header = document.getElementById("navigation");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=f5597f72a3504f64f27f4c96c72b88cc&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+
+
+    /*const days = jsonObject['sunday', 'Monday', 'Teasday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    /*let days = document.createElement('section')
+    /*let forecast = document.createElement('section');
+    /*let icon = document.createElement('icon');
+    /*let degree = document.createElement('')*/
+    
+    document.getElementById('currently').textContent = jsObject.list[4].weather[0].description;
+    document.getElementById('high').textContent = jsObject.list[4].main.temp;
+    document.getElementById('wind').textContent = jsObject.list[4].wind.deg;
+    document.getElementById('humidity').textContent = jsObject.list[4].main.humidity;
+    document.getElementById('speed').textContent = jsObject.list[4].wind.speed;
+
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.list[0].weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
   });
-}*/
+  
+
+
+  
